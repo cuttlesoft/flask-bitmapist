0ce8819f5c63e0a53b2c740b4afd60c572e58493
Katie Russ <katie@cuttlesoft.com>
2016-08-09 11:11:39 -0400

    Starter for requesting and returning very basic bitmapist cohort

--- flask_bitmapist/views.py
+++ flask_bitmapist/views.py
@@ -8,13 +8,13 @@
     :license: MIT, see LICENSE for more details.
 """
 
-import os
+import json, os
 
 from datetime import datetime
 
-from bitmapist import get_event_names, DayEvents, WeekEvents, MonthEvents, YearEvents
+from flask import Blueprint, jsonify, render_template, request
 
-from flask import Blueprint, render_template
+from bitmapist import get_event_names, DayEvents, WeekEvents, MonthEvents, YearEvents  # BitOpAnd, BitOpOr
 
 
 root = os.path.abspath(os.path.dirname(__file__))
@@ -44,9 +44,58 @@ def index():
     week_events = len(list(WeekEvents('user_logged_in', now.year, now.isocalendar()[1])))
     month_events = len(list(MonthEvents('user_logged_in', now.year, now.month)))
     year_events = len(list(YearEvents('user_logged_in', now.year)))
-    return render_template('bitmapist/data.html', events=get_event_names(), day_events=day_events, week_events=week_events, month_events=month_events, year_events=year_events)
+    # return render_template('bitmapist/data.html', ...
+    return render_template('bitmapist/index.html', events=get_event_names(), day_events=day_events, week_events=week_events, month_events=month_events, year_events=year_events)
 
 
 @bitmapist_bp.route('/events')
 def events():
-    return str(get_event_names())
+    now = datetime.utcnow()
+    event_names = get_event_names()
+
+    # For temporary listing of totals per event
+    events = {}
+    for event_name in event_names:
+        # TODO: + hourly
+        day = len(DayEvents(event_name, now.year, now.month, now.day))
+        week = len(WeekEvents(event_name, now.year, now.isocalendar()[1]))
+        month = len(MonthEvents(event_name, now.year, now.month))
+        year = len(YearEvents(event_name, now.year))
+        event = (year, month, week, day)
+        events[event_name] = event
+
+    intervals = ['year', 'month', 'week', 'day']
+    return render_template('bitmapist/events.html', event_names=event_names, intervals=intervals, events=events)
+
+@bitmapist_bp.route('/cohort', methods=['POST'])
+def cohort():
+    data = json.loads(request.data)
+
+    # SO RUFF... UoxoU
+    event = data.get('event')
+    ops = data.get('ops')
+
+    cohort = bitmapist_events(event.get('name'), event.get('range'))
+
+    if ops:
+        for op in ops:
+            if op.get('operation') == 'and':
+                cohort = cohort & bitmapist_events(op.get('name'), op.get('range'))
+            elif op.get('operation') == 'or':
+                cohort = cohort | bitmapist_events(op.get('name'), op.get('range'))
+
+    # print list(cohort)
+    return jsonify(list(cohort))
+
+def bitmapist_events(event_name, date_range):
+    now = datetime.utcnow()
+
+    # TODO: + hourly
+    if date_range == 'year':
+        return YearEvents(event_name, now.year)
+    elif date_range == 'month':
+        return MonthEvents(event_name, now.year, now.month)
+    elif date_range == 'week':
+        return WeekEvents(event_name, now.year, now.isocalendar()[1])
+    elif date_range == 'day':
+        return DayEvents(event_name, now.year, now.month, now.day)
